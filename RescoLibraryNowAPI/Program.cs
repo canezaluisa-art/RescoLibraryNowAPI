52b5ae0e9c1a6468e9e1d381924013bbd134f825
Administrator <Administrator@DESKTOP-1ULGF16>
2026-05-15 17:06:51 +0800

    Fix missing semicolon

--- RescoLibraryNowAPI/Program.cs
+++ RescoLibraryNowAPI/Program.cs
@@ -27,6 +27,6 @@ var summaries = new[]
 
 app.MapControllers();
 
-app.MapGet("/", () => "RescoLibraryNowAPI is running. Go to /swagger for API docs.")
+app.MapGet("/", () => "RescoLibraryNowAPI is running. Go to /swagger for API docs.");
 
 app.Run();
